23046bb60457e0838441fc9e689f685bdf9d396c
lishaojiang <lsjlhls@sina.com>
2019-06-02 09:57:05 +0800

    my self a circle ,test success

--- link/AFindMap.hpp
+++ link/AFindMap.hpp
@@ -4,28 +4,40 @@
 
 #include <vector>
 
-typedef unsigned char BYTE;
 
+typedef unsigned char BYTE;
 struct Point
 {
     Point ()
     {
         iX = 0;
         iY = 0;
-        iCost = INT_MAX;
+        iHaveCost = INT_MAX;
+        fCost = INT_MAX;
     }
 
-    Point(int X,int Y ,int Cost)
+    Point(int X,int Y ,int HaveCost, float Cost)
     {
         iX = X;
         iY = Y;
-        iCost = Cost;
+        iHaveCost = HaveCost;
+        fCost = Cost;
     }
 
     int iX;
     int iY;
-    int iCost;
+    int iHaveCost;
+    float fCost;
 };
+
+bool STLComparePoint(const Point & rkLeft,const Point & rkRight)
+{
+    if((rkLeft.fCost + rkLeft.iHaveCost) < (rkRight.fCost + rkRight.iHaveCost))
+    {
+        return true;
+    }
+    return false;
+}
 
 struct Grid
 {
@@ -64,23 +76,38 @@ public:
 
     void FindPath(int iStartX,int iStartY,int iEndX,int iEndY);
 
-    void SetBlock(int iX,int iY,BYTE iFlag);
-    BYTE GetBlock(int iX,int iY);
+    void SetGridBlock(int iX,int iY,BYTE iFlag);
+    BYTE GetGridBlock(int iX,int iY);
+    void SetGridCost(int iX,int iY,int iCost);
+    int GetGridCost(int iX,int iY);
+
+    float GetDistance(int iX,int iY,int iEndX,int iEndY);
 
     void PrintPath();
 
     bool IsInClostList(int iX,int iY);
     bool IsInOpenList(int iX,int iY);
-
+    void ChangeOpenListCost(int iX,int iY,int iCost);
+    void AddPointInOpenList(int iX,int iY,int iHaveCost,std::vector<Point>& rkList);
+    void RemovePointFromOpenList(int iX,int iY);
+    void AddPointInCloseList(int iX,int iY);
+    void RemovePointFromCloseList(int iX,int iY);
+    bool CheckOnePointToFindPath(int iX,int iY,int iNowCost);
+    bool IsInGridInPath(int iX,int iY);
 
 
 private:
     int m_iMapWidth;
     int m_iMapHeight;
+    int m_iStartX;
+    int m_iStartY;
+    int m_iEndX;
+    int m_iEndY;
     bool m_bCanObliqueMove;                 // can move obliqued
     std::vector<Grid> m_kMapGrid;
     std::vector<Point> m_kOpenList;
     std::vector<Point> m_kCloseList;
+    std::vector< std::vector<Point> > m_kPathList;
 };
 
 #endif
