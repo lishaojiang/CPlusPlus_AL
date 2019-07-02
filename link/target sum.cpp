0976718cb6186a41afb8a885990d1d5e3c956206
lishaojiang <lsjlhls@sina.com>
2019-07-02 23:13:57 +0800

    i can't do it by DP

--- link/target sum.cpp
+++ link/target sum.cpp
@@ -68,3 +68,21 @@ public:
         return findWay(nums,S,0);
     }
 };
+
+
+// internet solution
+class Solution {
+private:
+  int subsetSum(vector<int> &nums, int S) {
+    vector<int> dp(S + 1, 0);
+    dp[0] = 1;
+    for (int n : nums)
+      for (int i = S; i >= n; --i) dp[i] += dp[i - n];
+    return dp[S];
+  }
+public:
+  int findTargetSumWays(vector<int>& nums, int S) {
+    int sum = accumulate(nums.begin(), nums.end(), 0);
+    return sum < S || (sum + S) % 2 ? 0 : subsetSum(nums, (sum + S) / 2);
+  }
+};
